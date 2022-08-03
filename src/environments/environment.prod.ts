b828f1cab44501a62456fba60e52d279e8121bdf
jquigley1003 <jeff.quigley@wavinghi.com>
2022-08-03 16:07:34 -0400

    configure firebase and hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+import { env } from './env';
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: env.firebase.apiKey,
+    projectId: env.firebase.projectId,
+    storageBucket: env.firebase.storageBucket,
+    messagingSenderId: env.firebase.messagingSenderId,
+    appId: env.firebase.appId,
+    measurementId: env.firebase.measurementId
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,15 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import { env } from './env';
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: env.firebase.apiKey,
+    projectId: env.firebase.projectId,
+    storageBucket: env.firebase.storageBucket,
+    messagingSenderId: env.firebase.messagingSenderId,
+    appId: env.firebase.appId,
+    measurementId: env.firebase.measurementId
+  }
 };
 
 /*
